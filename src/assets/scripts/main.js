3cb43ab0884ddc2eb4cd11c83418e4d8d6823de3
Anxela Redondo <angela.redondo@zerintia.com>
2021-10-17 14:10:54 +0200

    clean idea portada

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -27,39 +27,74 @@
   window.addEventListener("scroll", onScroll);
 
   function onScroll() {
+    checkPosition();
+    slidersEffect();
+  }
+  function checkPosition() {
+    // document.querySelector(
+    //   ".sky"
+    // ).style.transform = `matrix(1,0,0,1,0,${calculateTransform(0, -200)})`;
+    document.querySelector(
+      ".desk"
+    ).style.transform = `matrix(1,0,0,1,0,${calculateTransform(0, -800)})`;
+
+    document.querySelector(
+      ".monitor"
+    ).style.transform = `matrix(1,0,0,1,0,${calculateTransform(0, 700, 345)})`;
+    document.querySelector(
+      ".plant"
+    ).style.transform = `matrix(1,0,0,1,0,${calculateTransform(50, 600, 270)})`;
+    // document.querySelector(
+    //   ".cloud3"
+    // ).style.transform = `matrix(1,0,0,1,0,${calculateTransform(-50, -650)})`;
+    // document.querySelector(
+    //   ".monitor"
+    // ).style.transform = `matrix(1,0,0,1,0,${calculateTransform(-10, -100)})`;
+    // document.querySelector(
+    //   ".mountMg"
+    // ).style.transform = `matrix(1,0,0,1,0,${calculateTransform(-30, -250)})`;
+    // document.querySelector(
+    //   ".mountFg"
+    // ).style.transform = `matrix(1,0,0,1,0,${calculateTransform(-50, -600)})`;
+  }
+
+  function calculateTransform(y1, y2, max) {
+    let scrollDist = document.querySelector(".scroll-dist");
+    let scrollDistV = scrollDist.getBoundingClientRect();
+    let calc = y1 + (Math.abs(scrollDistV.top) * y2) / (scrollDistV.height / 2);
+    return calc > max ? max : calc;
+  }
+  function slidersEffect() {
+    let slidersContainer = document.querySelector(".sliders-container");
     let slide1 = document.querySelector(".slide-1");
     let slide2 = document.querySelector(".slide-2");
     let slide3 = document.querySelector(".slide-3");
     let sliderBar = document.querySelector(".slider-bar .bar");
-
-    let boundingRectSlide1 = slide1.getBoundingClientRect();
-    let boundingRectSlide2 = slide2.getBoundingClientRect();
     let slidersWrapper = document.querySelector(".sliders-wrapper");
 
-    if (
-      boundingRectSlide1.top <= 0 &&
-      slidersWrapper.offsetTop > boundingRectSlide1.height / 2
-    ) {
+    let percentage =
+      ((slidersWrapper.offsetTop + slidersWrapper.offsetHeight) /
+        slidersContainer.getBoundingClientRect().height) *
+      100;
+
+    sliderBar.style.background = `linear-gradient(to right, black ${percentage}%, transparent ${percentage}%)`;
+    if (percentage > 40) {
       slide1.classList.remove("is-active");
       slide2.classList.add("is-active");
-      sliderBar.classList.replace('is-active-1', 'is-active-2');
-      if (
-        boundingRectSlide2.top <= 0 &&
-        slidersWrapper.offsetTop >
-          boundingRectSlide1.height + boundingRectSlide2.height / 2
-      ) {
+      sliderBar.classList.replace("is-active-1", "is-active-2");
+      if (percentage > 70) {
         slide2.classList.remove("is-active");
         slide3.classList.add("is-active");
-        sliderBar.classList.replace('is-active-2', 'is-active-3');
+        sliderBar.classList.replace("is-active-2", "is-active-3");
       } else {
         slide3.classList.remove("is-active");
         slide2.classList.add("is-active");
-        sliderBar.classList.replace('is-active-3', 'is-active-2');
+        sliderBar.classList.replace("is-active-3", "is-active-2");
       }
     } else {
       slide2.classList.remove("is-active");
       slide1.classList.add("is-active");
-      sliderBar.classList.replace('is-active-2', 'is-active-1');
+      sliderBar.classList.replace("is-active-2", "is-active-1");
     }
   }
 })();
